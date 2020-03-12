204d755563a9daf6127a9749a4ffcf6fad36fcf8
kTindalland <kaitindall@outlook.com>
2020-03-12 11:46:47 +0000

    Made some assets and put the splash screen on

--- WheelsForHire/ViewModels/HomeViewModel.cs
+++ WheelsForHire/ViewModels/HomeViewModel.cs
@@ -8,32 +8,10 @@ namespace WheelsForHire.ViewModels
 {
     public class HomeViewModel : BindableBase
     {
-        private string _testLabel;
-
-        public string TestLabel
-        {
-            get { return _testLabel; }
-            set 
-            { 
-                _testLabel = value;
-                RaisePropertyChanged();
-            }
-        }
 
         public HomeViewModel()
         {
-            TestLabel = "Hello world!";
-
-            var mytimer = new Timer();
-            mytimer.Interval = 3000;
-            mytimer.Elapsed += OnTimer;
-            mytimer.Start();
-        }
-
-        private void OnTimer(object sender, ElapsedEventArgs e)
-        {
-            ((Timer)sender).Stop();
-            TestLabel = "The timer finished :)";
+            
         }
     }
 }
