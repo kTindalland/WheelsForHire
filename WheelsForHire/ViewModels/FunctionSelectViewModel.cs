a172535e4056e023fa352872f432b9e690e1f72d
kTindalland <kaitindall@outlook.com>
2020-06-16 11:19:49 +0100

    Designed function select screen

--- WheelsForHire/ViewModels/FunctionSelectViewModel.cs
+++ WheelsForHire/ViewModels/FunctionSelectViewModel.cs
@@ -1,12 +1,31 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace WheelsForHire.ViewModels
 {
     public class FunctionSelectViewModel : BindableBase
     {
+        private readonly IRegionManager _regionManager;
 
+        public ICommand BackToSplashCommand { get; set; }
+
+        public FunctionSelectViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            BackToSplashCommand = new DelegateCommand(BackToSplash);
+        }
+
+        private void BackToSplash()
+        {
+            _regionManager.RequestNavigate("MainRegion", "Home");
+        }
     }
+
+
 }
