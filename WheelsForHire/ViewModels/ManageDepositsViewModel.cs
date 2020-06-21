aaab1fef26c688b7bd6b38a782bb88d72ad15ef6
Kai Tindall <kaitindall2000@gmail.com>
2020-06-21 13:38:09 +0100

    Did a lot of work on managing deposits

---
+++ WheelsForHire/ViewModels/ManageDepositsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Database;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace WheelsForHire.ViewModels
+{
+    public class ManageDepositsViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+        private readonly WheelsContext _context;
+
+        public ICommand BackCommand { get; set; }
+        public ICommand SearchUserCommand { get; set; }
+        public ICommand ShowUnpaidCommand { get; set; }
+        public ICommand ShowNotRefundedCommand { get; set; }
+        
+
+        public ManageDepositsViewModel(
+            IRegionManager regionManager,
+            WheelsContext context)
+        {
+            _regionManager = regionManager;
+            _context = context;
+        }
+
+    }
+}
